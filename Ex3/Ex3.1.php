46b0459410c026a5d36a46ec9055d9b18f144812
JanWojtowski <s26978@pjwstk.edu.pl>
2023-03-09 16:29:24 +0100

    Ex3.1

--- Ex3/Ex3.1.php
+++ Ex3/Ex3.1.php
@@ -1,3 +1,25 @@
 <?php
 
-echo ('tak');
+function createTab(){
+
+    for($i = 0; $i < 10; $i++){
+        $tab[$i] = rand(1,10);
+    }
+
+    return $tab;
+}
+
+function give_argument($tab,$x){
+    return $tab[$x];
+}
+
+$x = rand(0,9);
+
+$tab = createTab();
+
+echo ("Podany index: " . ($x+1));
+echo ("<br/>");
+echo ("Wygenerowana tablica:" . "<br/>");
+echo ("[" . implode(", ", $tab) . "]");
+echo ("<br/>");
+echo ("Wartość na danej pozycji: " . give_argument($tab,$x));
