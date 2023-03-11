4bc27150ed9938b365bf445ce15f3f5a5aa318fd
JanWojtowski <120735941+JanWojtowski@users.noreply.github.com>
2023-03-12 00:09:28 +0100

    Ex 4.5

--- Ex4/4.5.php
+++ Ex4/4.5.php
@@ -3,6 +3,7 @@
 $player = 1;
 $tab = [[null,null,null],[null,null,null],[null,null,null]];
 $move = [];
+$movec = 1;
 
 function ruch($player,$move,$tab){
     if ($tab[$move[0]][$move[1]] == null){
@@ -31,36 +32,36 @@ function printBoard($tab){
     echo (" 1 | " . $tab[2][0] . " | " . $tab[2][1] . " | " . $tab[2][2]);
 }
 
-function move($move){
-    if($move == 1){
+function move($move,$movec){
+    if($movec == 1){
         $move[0] = 1;
         $move[1] = 1;
     }
-    elseif ($move == 2){
+    elseif ($movec == 2){
         $move[0] = 2;
         $move[1] = 2;
     }
-    elseif ($move == 3){
+    elseif ($movec == 3){
         $move[0] = 0;
         $move[1] = 2;
     }
-    elseif ($move == 4){
+    elseif ($movec == 4){
         $move[0] = 2;
         $move[1] = 0;
     }
-    elseif ($move == 5){
+    elseif ($movec == 5){
         $move[0] = 2;
         $move[1] = 1;
     }
-    elseif ($move == 6){
+    elseif ($movec == 6){
         $move[0] = 0;
         $move[1] = 0;
     }
-    elseif ($move == 7){
+    elseif ($movec == 7){
         $move[0] = 0;
         $move[1] = 1;
     }
-    elseif ($move == 8){
+    elseif ($movec == 8){
         $move[0] = 1;
         $move[1] = 2;
     }
@@ -68,8 +69,34 @@ function move($move){
         $move[0] = 1;
         $move[1] = 0;
     }
-    $move++;
-    return $move;
+    $movec++;
+    return $movec;
 }
 
+function checkVictory($tab){
+    for($i = 0; $i < 3; $i++){
+        if($tab[$i][0] == $tab[$i][1] and $tab[$i][1] == $tab[$i][2] and $tab[$i][0] != null){
+            return true;
+        }
+        if($tab[0][$i] == $tab[1][$i] and $tab[1][$i] == $tab[2][$i] and  $tab[0][$i] != null){
+            return true;
+        }
+    }
+    if ($tab[0][0] == $tab[1][1] and $tab[1][1] == $tab[2][2] and $tab[0][0] != null){
+        return true;
+    }
+    if ($tab[0][2] == $tab[1][1] and $tab[1][1] == $tab[2][2] and $tab[0][2] != null){
+        return true;
+    }
+    return false;
+}
+
+while(true){
+    $movec = move($move,$movec);
+    $player = ruch($player,$move,$tab);
+    printBoard($tab);
+    if (checkVictory($tab)){
+        break;
+    }
+}
 
