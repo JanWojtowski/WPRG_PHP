8247478cefb906315d8808cb000a1bf9ae816216
JanWojtowski <120735941+JanWojtowski@users.noreply.github.com>
2023-03-12 00:40:51 +0100

    Ex 4.5

--- Ex4/4.5.php
+++ Ex4/4.5.php
@@ -1,35 +1,34 @@
 <?php
 
 $player = 1;
-$tab = [[null,null,null],[null,null,null],[null,null,null]];
-$move = [];
-$movec = 1;
+$tab = [["..","..",".."],["..","..",".."],["..","..",".."]];
+$move = [1,1];
 
 function ruch($player,$move,$tab){
-    if ($tab[$move[0]][$move[1]] == null){
+    if ($tab[$move[0]][$move[1]] == "."){
         if($player == 1){
             $tab[$move[0]][$move[1]] = "o";
-            $player = 2;
         }
         else{
             $tab[$move[0]][$move[1]] = "x";
-            $player = 1;
         }
     }
     else{
         echo "Miejsce zajęte";
     }
-    return $player;
+    return $tab;
 }
 
 function printBoard($tab){
+    echo ("<br/>");
     echo (" 0 | 1 | 2 | 3 |");
     echo ("<br/>");
-    echo (" 1 | " . $tab[0][0] . " | " . $tab[0][1] . " | " . $tab[0][2]);
+    echo (" 1 | " . $tab[0][0] . " | " . $tab[0][1] . " | " . $tab[0][2] . " |");
     echo ("<br/>");
-    echo (" 1 | " . $tab[1][0] . " | " . $tab[1][1] . " | " . $tab[1][2]);
+    echo (" 2 | " . $tab[1][0] . " | " . $tab[1][1] . " | " . $tab[1][2] . " |");
     echo ("<br/>");
-    echo (" 1 | " . $tab[2][0] . " | " . $tab[2][1] . " | " . $tab[2][2]);
+    echo (" 3 | " . $tab[2][0] . " | " . $tab[2][1] . " | " . $tab[2][2] . " |");
+    echo ("<br/>");
 }
 
 function move($move,$movec){
@@ -65,38 +64,90 @@ function move($move,$movec){
         $move[0] = 1;
         $move[1] = 2;
     }
-    else{
+    elseif ($movec == 9){
         $move[0] = 1;
         $move[1] = 0;
     }
-    $movec++;
-    return $movec;
 }
 
 function checkVictory($tab){
     for($i = 0; $i < 3; $i++){
-        if($tab[$i][0] == $tab[$i][1] and $tab[$i][1] == $tab[$i][2] and $tab[$i][0] != null){
+        if($tab[$i][0] == $tab[$i][1] and $tab[$i][1] == $tab[$i][2] and $tab[$i][0] != ".."){
             return true;
         }
-        if($tab[0][$i] == $tab[1][$i] and $tab[1][$i] == $tab[2][$i] and  $tab[0][$i] != null){
+        if($tab[0][$i] == $tab[1][$i] and $tab[1][$i] == $tab[2][$i] and  $tab[0][$i] != ".."){
             return true;
         }
     }
-    if ($tab[0][0] == $tab[1][1] and $tab[1][1] == $tab[2][2] and $tab[0][0] != null){
+    if ($tab[0][0] == $tab[1][1] and $tab[1][1] == $tab[2][2] and $tab[0][0] != ".."){
         return true;
     }
-    if ($tab[0][2] == $tab[1][1] and $tab[1][1] == $tab[2][2] and $tab[0][2] != null){
+    if ($tab[0][2] == $tab[1][1] and $tab[1][1] == $tab[2][2] and $tab[0][2] != ".."){
         return true;
     }
     return false;
 }
 
-while(true){
-    $movec = move($move,$movec);
-    $player = ruch($player,$move,$tab);
+for($i = 1; $i < 10; $i++){
+    if($i == 1){
+        $move[0] = 1;
+        $move[1] = 1;
+    }
+    elseif ($i == 2){
+        $move[0] = 2;
+        $move[1] = 2;
+    }
+    elseif ($i == 3){
+        $move[0] = 0;
+        $move[1] = 2;
+    }
+    elseif ($i == 4){
+        $move[0] = 2;
+        $move[1] = 0;
+    }
+    elseif ($i == 5){
+        $move[0] = 2;
+        $move[1] = 1;
+    }
+    elseif ($i == 6){
+        $move[0] = 0;
+        $move[1] = 0;
+    }
+    elseif ($i == 7){
+        $move[0] = 0;
+        $move[1] = 1;
+    }
+    elseif ($i == 8){
+        $move[0] = 1;
+        $move[1] = 2;
+    }
+    elseif ($i == 9){
+        $move[0] = 1;
+        $move[1] = 0;
+    }
+    if($i % 2 == 1){
+        $player = 1;
+    }
+    else{
+        $player = 2;
+    }
+    if ($tab[$move[0]][$move[1]] == ".."){
+        if($player == 1){
+            $tab[$move[0]][$move[1]] = "o";
+        }
+        else{
+            $tab[$move[0]][$move[1]] = "x";
+        }
+    }
+    else{
+        echo "Miejsce zajęte";
+    }
     printBoard($tab);
     if (checkVictory($tab)){
+        echo ("Gracz $player wygrał!!!");
         break;
     }
 }
+
+
 
