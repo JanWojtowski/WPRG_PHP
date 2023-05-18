a73fe7e38a13d1c061aec5aa8116d57a91ff46ca
JanWojtowski <s26978@pjwstk.edu.pl>
2023-05-18 18:23:03 +0200

    Lab10

---
+++ Lab10/Ex1/admin.php
@@ -0,0 +1,163 @@
+<?php
+session_start();
+
+$dbuser = 's26978';
+$dbpass = 'Jan.Wojt';
+$db = new PDO("mysql:host=localhost;dbname=s26978", $dbuser,$dbpass);
+
+if(!isset($_SESSION['id'])){
+    if(isset($_POST['login'])){
+        $query = "SELECT * FROM user where login = '"  . $_POST["login"] . "'";
+        $result = $db->query($query);
+        if (!$result){
+            header("location: admin.php");
+        }
+        while($row = $result->fetch(PDO::FETCH_ASSOC)){
+            if($_POST['password'] == $row['password']){
+                $_SESSION['id'] = $row['id'];
+            }
+            else{
+                $_SESSION['error'] = "Niepoprawne hasło!!!";
+                header("location: admin.php");
+                exit();
+            }
+        }
+        $_SESSION['error'] = "Niepoprawny login!!!";
+        header("location: admin.php");
+    }
+    else{
+        if(isset($_SESSION['error'])){
+            echo $_SESSION['error'];
+        }
+         ?>
+        <html>
+            <body>
+                <form action="admin.php" method="post">
+                    <label for="login">Enter login:</label>
+                    <input type="text" name="login" id="login" size="20">
+                    <br/>
+                    <label for="password">Enter password:</label>
+                    <input type="text" name="password" id="password" size="20">
+                    <br/>
+                    <input type="submit" name="Send" value="send">
+                </form>
+            </body>
+        </html>
+        <?php
+    }
+}
+else{
+    if(isset($_GET["info"])){
+        $query = "SELECT * FROM cars where id = " . $_GET["info"];
+        $result = $db->query($query);
+        if (!$result){
+            echo "BŁĄD w zapytaniu";
+            exit;
+        }
+        while($row = $result->fetch(PDO::FETCH_ASSOC)){
+            echo "Id oferty: " . $row["id"];
+            echo "</br>";
+            echo "Marka: " . $row["marka"];
+            echo "</br>";
+            echo "Model: " . $row["model"];
+            echo "</br>";
+            echo "Rocznik: " . $row["rok"];
+            echo "</br>";
+            echo "Cena: " . $row["cena"];
+            echo "</br>";
+            echo "Opis: " . $row["opis"];
+            echo "</br>";
+            ?>
+            <html>
+            <body>
+            <?php
+            echo "<a href=admin.php?strona=" . $_GET["strona"] . ">" . "Powrot" . "</a>"
+            ?>
+            </body>
+            </html>
+            <?php
+        }
+    }
+    elseif($_GET["edit"]){
+        $query = "SELECT * FROM cars where id = " . $_GET["edit"];
+        $result = $db->query($query);
+        if (!$result){
+            echo "BŁĄD w zapytaniu";
+            exit;
+        }
+        while($row = $result->fetch(PDO::FETCH_ASSOC)){
+            if($row["user_id"] != $_SESSION["id"]){
+                $_SESSION["error"] = "Musisz być zalogowny by edytowac samochód";
+                unset($_SESSION["id"]);
+                header("location: admin.php");
+                exit();
+            }
+            elseif(isset($_GET["upl"])){
+
+            }
+            else{
+                ?>
+                    <html>
+                        <body>
+                            <form action="<?php echo "admin.php?edit=" . $_GET["edit"] . "&upl=" . $row["id"] . "&strona=" . $_GET["strona"];?>">
+                                <label for="marka">Marka:</label><br>
+                                <input type="text" id="marka" name="marka" value="<?php echo $row["marka"]; ?>"><br>
+                                <label for="model">Model:</label><br>
+                                <input type="text" id="model" name="model" value="<?php echo $row["model"]; ?>"><br>
+                                <label for="cena">Cena:</label><br>
+                                <input type="text" id="cena" name="cena" value="<?php echo $row["cena"]; ?>"><br>
+                                <label for="rok">Rok produkcji:</label><br>
+                                <input type="text" id="rok" name="rok" value="<?php echo $row["rok"]; ?>"><br>
+                                <label for="opis">Opis:</label><br>
+                                <input type="text" id="opis" name="opis" value="<?php echo $row["opis"]; ?>"><br>
+                            </form>
+                        </body>
+                    </html>
+                <?php
+            }
+        }
+
+    }
+    else{
+        if(!isset($_GET['strona'])){
+            header("location: admin.php?strona=0");
+        }
+
+        $limit = 5;
+        $offset = $_GET['strona'] * $limit;
+
+        $query = "SELECT * FROM cars WHERE user_id =" . $_SESSION['id'] . " LIMIT " . $limit . " OFFSET " . $offset;
+        $result = $db->query($query);
+        if (!$result){
+            echo "BŁĄD w zapytaniu";
+            exit;
+        }
+        while($row = $result->fetch(PDO::FETCH_ASSOC)){
+            echo $row["id"] . "    " . $row["marka"] . "    " . $row["model"];
+            ?>
+            <html>
+            <body>
+            <?php
+            echo "<a href=admin.php?info=" . $row["id"] . "&strona=" . $_GET["strona"] . "> Wiecej informacji </a>";
+            echo "<a href=admin.php?edit=" . $row["id"] . "&strona=" . $_GET["strona"] . "> Wiecej informacji </a>";
+            ?>
+            </body>
+            </html>
+            <?php
+            echo "</br>";
+        }
+        ?>
+        <html>
+        <body>
+        <?php
+        if($_GET["strona"] != 0){
+            echo "<a href=admin.php?strona=" . ($_GET["strona"] - 1) . "> <--Poprzednia </a>";
+        }
+        echo "    Aktualna strona:" . $_GET["strona"];
+        echo "<a href=admin.php?strona=" . ($_GET["strona"] + 1) . "> Następna--> </a>";
+        ?>
+        </body>
+        </html>
+        <?php
+    }
+}
---
+++ Lab10/Ex1/index.php
@@ -0,0 +1,79 @@
+<?php
+$dbuser = 's26978';
+$dbpass = 'Jan.Wojt';
+$db = new PDO("mysql:host=localhost;dbname=s26978", $dbuser,$dbpass);
+
+if(isset($_GET["info"])){
+    $query = "SELECT * FROM cars where id = " . $_GET["info"];
+    $result = $db->query($query);
+    if (!$result){
+        echo "BŁĄD w zapytaniu";
+        exit;
+    }
+    while($row = $result->fetch(PDO::FETCH_ASSOC)){
+        echo "Id oferty: " . $row["id"];
+        echo "</br>";
+        echo "Marka: " . $row["marka"];
+        echo "</br>";
+        echo "Model: " . $row["model"];
+        echo "</br>";
+        echo "Rocznik: " . $row["rok"];
+        echo "</br>";
+        echo "Cena: " . $row["cena"];
+        echo "</br>";
+        echo "Opis: " . $row["opis"];
+        echo "</br>";
+        ?>
+        <html>
+        <body>
+        <?php
+        echo "<a href=index.php?strona=" . $_GET["strona"] . ">" . "Powrot" . "</a>"
+        ?>
+        </body>
+        </html>
+        <?php
+    }
+}
+else{
+    if(!isset($_GET['strona'])){
+        header("location: index.php?strona=0");
+    }
+
+    $limit = 5;
+    $offset = $_GET['strona'] * $limit;
+
+    $query = "SELECT * FROM cars LIMIT " . $limit . " OFFSET " . $offset;
+    $result = $db->query($query);
+    if (!$result){
+        echo "BŁĄD w zapytaniu";
+        exit;
+    }
+    while($row = $result->fetch(PDO::FETCH_ASSOC)){
+        echo $row["id"] . "    " . $row["marka"] . "    " . $row["model"];
+        ?>
+        <html>
+        <body>
+        <?php
+        echo "<a href=index.php?info=" . $row["id"] . "&strona=" . $_GET["strona"] . "> Wiecej informacji </a>"
+        ?>
+        </body>
+        </html>
+        <?php
+        echo "</br>";
+    }
+    ?>
+    <html>
+    <body>
+    <?php
+    if($_GET["strona"] != 0){
+        echo "<a href=index.php?strona=" . ($_GET["strona"] - 1) . "> <--Poprzednia </a>";
+    }
+    echo "    Aktualna strona:" . $_GET["strona"];
+    echo "<a href=index.php?strona=" . ($_GET["strona"] + 1) . "> Następna--> </a>";
+    ?>
+    </body>
+    </html>
+    <?php
+}
+
+
